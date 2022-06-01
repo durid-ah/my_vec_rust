cfe625e12bb2b487af28bbf607cf2239171e356a
durid-ah <ahmad.durid96@gmail.com>
2022-05-31 20:57:22 -0600

    wrapping up the allocating section

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,4 +1,4 @@
-use std::{ptr::NonNull, marker::PhantomData, mem};
+use std::{ptr::NonNull, marker::PhantomData, mem, alloc::{self, Layout}};
 
 pub struct  Vec<T> {
    ptr: NonNull<T>,
@@ -16,5 +16,35 @@ impl<T> Vec<T> {
       Vec { ptr: NonNull::dangling(), cap: 0, len: 0, _marker: PhantomData }
    }
 
-    
+    fn grow(&mut self) {
+      let (new_cap, new_layout) = if self.cap == 0 {
+         (1, Layout::array::<T>(1).unwrap())
+      } else {
+         // This can't overflow since self.cap <= isize::MAX
+         let new_cap = 2 * self.cap;
+
+         // `Layout::array` checks that the number of bytes is <= usize::MAX,
+         // but this is redundant since old_layout.size() <= isize::MAX,
+         // so the `unwrap` should never fail.
+         let new_layout = Layout::array::<T>(new_cap).unwrap();
+         (new_cap, new_layout)
+      };
+
+      // Ensure that the new allocation doesn't exceed `isize::MAX` bytes.
+      assert!(new_layout.size() <= isize::MAX as usize, "Allocation too large");
+      
+      let new_ptr = if self.cap == 0 {
+          unsafe { alloc::alloc(new_layout) }
+      } else {
+          let old_layout = Layout::array::<T>(self.cap).unwrap();
+          let old_ptr = self.ptr.as_ptr() as *mut u8;
+          unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
+      };
+
+      // If allocation fails, `new_ptr` will be null, in which case we abort.
+      self.ptr = match NonNull::new(new_ptr as *mut T) {
+         Some(p) => p,
+         None => alloc::handle_alloc_error(new_layout),
+      };
+    }
 }
